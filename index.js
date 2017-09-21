0c904638f0be7f5e567e042d33559eba82a9bf4d
Othman AMD <red.fantastic@hotmail.com>
2017-09-21 09:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
+<<<<<<< HEAD
 
 var animals = ["dog", "cat", "squirrel"];
 
@@ -13,3 +14,12 @@ doToElementsInArray(animals, changeCompletely);
 
 // log out animals -- pretty cool, right?
 console.log(animals);
+=======
+ 
+var animals = ["dog", "cat", "squirrel"];
+ 
+doToElementsInArray(animals, changeCompletely);
+ 
+// log out animals -- pretty cool, right?
+console.log(animals);
+>>>>>>> 8691a991e20f8bae9a37f29fa802dd959089f8b9
